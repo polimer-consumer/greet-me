25320c46a506487953adc431f70e753a4ed337e0
Evgenii <jenyhlevnoy@gmail.com>
2023-12-28 15:26:56 +0200

    Deleted resource bundle, few tweaks to plugin

--- src/main/kotlin/com/github/polimerconsumer/greetme/actions/GreetAction.kt
+++ src/main/kotlin/com/github/polimerconsumer/greetme/actions/GreetAction.kt
@@ -7,9 +7,24 @@ import com.intellij.openapi.ui.Messages
 
 class GreetAction : AnAction() {
     override fun actionPerformed(actionEvent: AnActionEvent) {
-        // val greetDialogWindow = GreetDialogWrapper(actionEvent.project)
+        val userName = Messages.showInputDialog(
+            actionEvent.project,
+            "What's your name, buddy?",
+            "Greet Me",
+            Messages.getQuestionIcon()
+        )
 
-        Messages.showMessageDialog(actionEvent.project, "Hello world", "Greet Me", Messages.getQuestionIcon())
-        // greetDialogWindow.showAndGet()
+        if (!userName.isNullOrEmpty()) {
+            val greetingMessage = "Hello, $userName!"
+            Messages.showMessageDialog(actionEvent.project, greetingMessage, "Greet Me", Messages.getInformationIcon())
+        } else {
+            Messages.showMessageDialog(actionEvent.project, "Hello, anonymous", "Greet Me", Messages.getInformationIcon())
+        }
     }
+//    override fun actionPerformed(actionEvent: AnActionEvent) {
+//        // val greetDialogWindow = GreetDialogWrapper(actionEvent.project)
+//
+//        Messages.showMessageDialog(actionEvent.project, "Hello world", "Greet Me", Messages.getQuestionIcon())
+//        // greetDialogWindow.showAndGet()
+//    }
 }
